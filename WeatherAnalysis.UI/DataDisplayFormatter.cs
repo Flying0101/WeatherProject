785be5f3adb0196ddbc4f524ffe0ff153e24f4e1
Stephano <stephano@live.se>
2024-11-24 00:14:55 +0100

    created: UI console app and added new classes

---
+++ WeatherAnalysis.UI/DataDisplayFormatter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherAnalysis.UI
+{
+    internal class DataDisplayFormatter
+    {
+    }
+}
